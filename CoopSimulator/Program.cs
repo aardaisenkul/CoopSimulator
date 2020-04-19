1b59ea3a11cfab6d66e3ca7fea0265c0ae3a7dd6
aardaisenkul <aarda.isenkul@tedu.edu.tr>
2020-04-19 18:56:05 +0300

    Coop Simulator from Configuration file 19.04.2020

---
+++ CoopSimulator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using CoopSimulator.Data;
+using Microsoft.VisualBasic;
+using System.Linq;
+
+namespace CoopSimulator
+{
+    class Program
+    {
+        public static PoultryHandler PoultryHandler = new PoultryHandler();
+        public static List<Data.PoultryDto> PoultryList = new List<PoultryDto>();
+
+        public static Data.ConfigurationDto.Configuration Configuration { get; set; }
+        public static Data.DateDto Date { get; set; }
+        static void Main(string[] args)
+        {
+
+            Configuration = Util.Configuration();
+            Date = new DateDto(Configuration.GlobalSetting.Reporting.DatePeriod, Configuration.GlobalSetting.Reporting.Value,DatePeriodTriggered)
+            {
+                Date = new DateTime(2000,1,1)
+            };
+            Console.WriteLine("WELCOME TO COOP SIMULATOR OF " + Configuration.PoultryDetail.Name);
+            Util.Init();
+            Console.ReadLine();
+        }
+        public static void DatePeriodTriggered(object sender, EventArgs args)
+        {
+            Console.WriteLine(Program.Date.Date.ToString("dd-MM-yyyy") + 
+                " | F: " + Program.PoultryList.Count(a => a.Sex == Enums.PoultrySex.Female)+
+                " | M: " + Program.PoultryList.Count(a => a.Sex == Enums.PoultrySex.Male)+ 
+                " | AVG: "+(int)Program.PoultryList.Average(a => (Program.Date.Date-a.BirthDate).TotalDays));
+        }
+    }
+}
